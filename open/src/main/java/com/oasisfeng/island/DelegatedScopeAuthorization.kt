oasisfeng/island

commit eaee3816542eaeba043bc985afd7f62e190ecdd1

UPDATE: Minor tweaks.

open/src/main/java/com/oasisfeng/island/DelegatedScopeAuthorization.kt
@@ -78,11 +78,13 @@ class DelegatedScopeAuthorization : RestrictionsReceiver() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        onRequestReactedByUser(context, when {
-            intent.action == ACTION_AUTHORIZE -> true
-            intent.action == ACTION_REFUSE    -> false
-            else                              -> return super.onReceive(context, intent)
-        }, intent.data?.schemeSpecificPart, intent.getStringExtra(EXTRA_PACKAGE_NAME), intent.getParcelableExtra(EXTRA_USER), intent.getStringExtra(REQUEST_KEY_DATA))
+        val pkg = intent.getStringExtra(EXTRA_PACKAGE_NAME) ?: return
+        val user : UserHandle = intent.getParcelableExtra(EXTRA_USER) ?: return
+        onRequestReactedByUser(context, when (intent.action) {
+            ACTION_AUTHORIZE -> true
+            ACTION_REFUSE -> false
+            else -> return super.onReceive(context, intent)
+        }, intent.data?.schemeSpecificPart, pkg, user, intent.getStringExtra(REQUEST_KEY_DATA) ?: return)
     }
 
     private fun onRequestReactedByUser(context: Context, authorized: Boolean, requestId: String?, pkg: String, user: UserHandle, delegation: String) {
@@ -105,11 +107,10 @@ class DelegatedScopeAuthorization : RestrictionsReceiver() {
         }
     }
 
-    private fun logAndToast(context: Context?, pkg: String?, message: String?) {
+    private fun logAndToast(context: Context?, pkg: String?, message: String) {
         Log.w(TAG, message)
         pkg?.let { Apps.of(context).getAppInfo(it) }?.flags?.apply { and(ApplicationInfo.FLAG_TEST_ONLY) != 0 }?.also {
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-        }
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show() }
     }
 
     class Initializer : PseudoContentProvider() { override fun onCreate(): Boolean {

build.gradle
@@ -25,6 +25,6 @@ allprojects {
         jcenter()
     }
 
-    ext.compileSdkVersion = 29
+    ext.compileSdkVersion = 30
     ext.minSdkVersion = 24
 }

mobile/build.gradle
@@ -2,7 +2,7 @@ apply plugin: 'com.android.library'
 apply plugin: 'kotlin-android'
 
 android {
-    compileSdkVersion 30
+    compileSdkVersion this.compileSdkVersion
 
     defaultConfig {
         minSdkVersion this.minSdkVersion

mobile/src/main/java/com/oasisfeng/island/model/AppListViewModel.java
@@ -191,6 +191,7 @@ public class AppListViewModel extends BaseAppListViewModel<AppViewModel> {
 			mFeatured.visible.setValue(Boolean.TRUE);
 			mFeatured.update(activity);
 			mChipsVisible.setValue(false);
+			Analytics.log(TAG, "tab-discover");
 			return;
 		} else mFeatured.visible.setValue(Boolean.FALSE);
 
@@ -201,6 +202,7 @@ public class AppListViewModel extends BaseAppListViewModel<AppViewModel> {
 				if (Users.isProfileManagedByIsland(profile)) {
 					setCurrentProfile(profile);
 					updateAppList();
+					Analytics.log(TAG, "tab-island-" + UserHandles.getIdentifier(profile));
 					return;
 				}
 			}
@@ -208,6 +210,7 @@ public class AppListViewModel extends BaseAppListViewModel<AppViewModel> {
 		tabs.selectTab(tabs.getTabAt(1));   // Switch back to Mainland
 		setCurrentProfile(Users.owner);
 		updateAppList();
+		Analytics.log(TAG, "tab-mainland");
 	}
 
 	public void updateActions(final Menu menu) {

mobile/src/main/java/com/oasisfeng/island/model/IslandViewModel.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.viewModelScope
 import com.oasisfeng.common.app.BaseAndroidViewModel
-import com.oasisfeng.island.analytics.analytics
+import com.oasisfeng.island.analytics.Analytics
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.future.future
@@ -23,6 +23,6 @@ fun <R> BaseAndroidViewModel.interactiveFuture(context: Context, block: suspend 
 
 private fun handleException(context: Context, tag: String, t: Throwable) {
 	if (t is CancellationException) return Unit.also { Log.i(tag, "Interaction canceled: ${t.message}") }
-	analytics().logAndReport(tag, "Unexpected internal error", t)
+	Analytics().logAndReport(tag, "Unexpected internal error", t)
 	Toast.makeText(context, "Internal error: " + t.message, Toast.LENGTH_LONG).show()
 }

mobile/src/main/java/com/oasisfeng/island/settings/IslandSettingsActivity.kt
@@ -2,7 +2,10 @@
 
 package com.oasisfeng.island.settings
 
-import android.Manifest.permission.*
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_PHONE_STATE
+import android.Manifest.permission.READ_SMS
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.admin.DevicePolicyManager.ACTION_PROVISION_MANAGED_DEVICE
@@ -91,7 +94,7 @@ class IslandSettingsFragment: android.preference.PreferenceFragment() {
                         .filter { it.requestedPermissions?.contains(INTERACT_ACROSS_PROFILES) == true }
                 val pm = activity.packageManager
                 val entries = pkgs.map { it.applicationInfo.loadLabel(pm) }.toTypedArray()
-                val allowedPackages = policies.invoke(DPM::getCrossProfilePackages)
+                val allowedPackages: Set<String> = policies.invoke(DPM::getCrossProfilePackages)
                 val allowed = BooleanArray(entries.size) { index -> pkgs[index].packageName in allowedPackages }
                 Dialogs.buildCheckList(activity, activity.getText(R.string.prompt_manage_cross_profile_apps),
                         entries, allowed) { _, which, checked -> allowed[which] = checked }.withOkButton {
@@ -99,7 +102,7 @@ class IslandSettingsFragment: android.preference.PreferenceFragment() {
                                     .toSet().also { policies.invoke(DPM::setCrossProfilePackages, it) }}
                         .withCancelButton().show() }}
 
-        setupNotificationChannelTwoStatePreference(R.string.key_island_watcher, SDK_INT >= P && ! Users.isOwner(), NotificationIds.IslandWatcher)
+        setupNotificationChannelTwoStatePreference(R.string.key_island_watcher, SDK_INT >= P && !Users.isOwner(), NotificationIds.IslandWatcher)
         setupNotificationChannelTwoStatePreference(R.string.key_app_watcher, SDK_INT >= O, NotificationIds.IslandAppWatcher)
 
         setup<Preference>(R.string.key_reprovision) {
@@ -128,7 +131,7 @@ class IslandSettingsFragment: android.preference.PreferenceFragment() {
         setup<TwoStatePreference>(key) {
             if (visible && SDK_INT >= O) {
                 isChecked = ! notificationId.isBlocked(context)
-                setOnPreferenceChangeListener { _,_ -> true.also { context.startActivity(notificationId.buildChannelSettingsIntent(context)) }}
+                setOnPreferenceChangeListener { _, _ -> true.also { context.startActivity(notificationId.buildChannelSettingsIntent(context)) }}
             } else remove(this)
         }
     }
@@ -149,11 +152,11 @@ class IslandSettingsFragment: android.preference.PreferenceFragment() {
                 if (text.isNullOrEmpty()) text = IslandNameManager.getDefaultName(activity)
                 editText.also { editText ->
                     editText.addTextChangedListener(object: TextWatcher {
-                    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-                    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+                        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
 
-                    override fun afterTextChanged(s: Editable) {
-                        if (editText.text.any { it < ' ' }) editText.error = getString(R.string.prompt_invalid_input) }})
+                        override fun afterTextChanged(s: Editable) {
+                            if (editText.text.any { it < ' ' }) editText.error = getString(R.string.prompt_invalid_input) }})
 
                 setOnPreferenceChangeListener { _, name -> (editText.error == null).also { if (it)
                     onIslandRenamed(name.toString()) }}
@@ -217,3 +220,5 @@ class IslandSettingsActivity: Activity() {
         }
     }
 }
+
+private const val INTERACT_ACROSS_PROFILES = "android.permission.INTERACT_ACROSS_PROFILES"

shared/src/main/java/android/app/NotificationManagerExtender.java
@@ -87,7 +87,6 @@ public class NotificationManagerExtender extends NotificationManager {
 	}
 
 	public NotificationManagerExtender(final Context context) {
-		super(context, new Handler());
 		mDelegate = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 	}
 

shared/src/main/java/com/oasisfeng/island/shuttle/PendingIntentShuttle.kt
@@ -96,7 +96,7 @@ class PendingIntentShuttle: BroadcastReceiver() {
 		private fun <R> Continuation<R>.resume(result: Int, extras: Bundle?, procedureClass: Class<*>? = null) = when (result) {
 			Activity.RESULT_OK  -> resume(@Suppress("UNCHECKED_CAST") (extras?.get(null) as R))
 			RESULT_FIRST_USER   -> resumeWithException(extras?.get(null) as Throwable)
-			else                -> resumeWithException(RuntimeException("Error shuttling ${procedureClass ?: ""}")) }
+			else                -> resumeWithException(RuntimeException("Error shuttling ${procedureClass ?: ""} (code $result)")) }
 
 		@ProfileUser fun sendToParentProfileByActivityIfNotYet(context: Context) {
 			if (mSentByActivity) return     // Activity start is relatively heavy and cross-profile toast will be shown.

dev-akashc/insular

commit ac2e9761307d127f80ad482abbd95344569e72f2

FIX: Restrictions request procedure is broken.
Bug introduced in commit eaee3816542eaeba043bc985afd7f62e190ecdd1.

open/src/main/java/com/oasisfeng/island/DelegatedScopeAuthorization.kt
@@ -14,7 +14,9 @@ import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.content.RestrictionsManager
 import android.content.RestrictionsManager.*
 import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager.*
+import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+import android.content.pm.PackageManager.DONT_KILL_APP
+import android.content.pm.PackageManager.GET_META_DATA
 import android.net.Uri
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.O
@@ -78,13 +80,15 @@ class DelegatedScopeAuthorization : RestrictionsReceiver() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        val pkg = intent.getStringExtra(EXTRA_PACKAGE_NAME) ?: return
-        val user : UserHandle = intent.getParcelableExtra(EXTRA_USER) ?: return
-        onRequestReactedByUser(context, when (intent.action) {
+        val authorized = when (intent.action) {
             ACTION_AUTHORIZE -> true
             ACTION_REFUSE -> false
-            else -> return super.onReceive(context, intent)
-        }, intent.data?.schemeSpecificPart, pkg, user, intent.getStringExtra(REQUEST_KEY_DATA) ?: return)
+            else -> return super.onReceive(context, intent) }
+        val pkg = intent.getStringExtra(EXTRA_PACKAGE_NAME) ?: return
+        val user : UserHandle = intent.getParcelableExtra(EXTRA_USER) ?: return
+        val delegation = intent.getStringExtra(REQUEST_KEY_DATA) ?: return
+        val requestId = intent.data?.schemeSpecificPart
+        onRequestReactedByUser(context, authorized, requestId, pkg, user, delegation)
     }
 
     private fun onRequestReactedByUser(context: Context, authorized: Boolean, requestId: String?, pkg: String, user: UserHandle, delegation: String) {